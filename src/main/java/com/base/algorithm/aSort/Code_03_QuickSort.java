8484ec21926c587226b1e80e45be48ae971d2402
zhoukl <kaile95@163.com>
2019-09-11 13:23:51 +0800

    快排

--- src/main/java/com/base/algorithm/aSort/Code_03_QuickSort.java
+++ src/main/java/com/base/algorithm/aSort/Code_03_QuickSort.java
@@ -1,5 +1,28 @@
 package com.base.algorithm.aSort;
 
+/**
+ * 时间复杂度：O(N*logN)
+ * 空间复杂度：O(log2N)
+ * 快排：
+ * 1、当前值(index) < 目标值, 当前值和小于区后一个数交换, 小于区向右扩一位, 当前值++;
+ * 2、当前值(index) == 目标值, 当前值后移(index++);
+ * 3、当前值(index) > 目标值, 当前值与大于区前一个数交换, 大于去向左扩一位, 当前值不变.
+ * 随机快排：
+ * 目标值随机产生,然后目标值和数组最后一位交换。
+ * @ClassName: Code_03_QuickSort
+ * @Author: zhoukl
+ * @Date: 2019/9/11 11:55
+ * @Version: 1.0
+ */
 public class Code_03_QuickSort {
 
+    /**
+     * 快排
+     * @param arr   目标数组
+     * @return  0
+     */
+    public static int sort_quick(int[] arr){
+
+        return 0;
+    }
 }
